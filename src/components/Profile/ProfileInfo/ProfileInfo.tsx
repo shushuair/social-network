c46b443161de450e89d945476899195cca27216f
shushuair <shushulkov1989@gmail.com>
2023-12-15 22:52:44 +0000

    fixed img

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -6,8 +6,7 @@ export const ProfileInfo = () => {
         <div className={s.content}>
             <div>
                 <img className={s.contentImage}
-                     src='src="https://thumbs.dreamstime.com/b/funchal-%C3%A2%E2%82%AC-madeira-island-portugal-85328664.jpg"
-                    alt=""/>'
+                     src={'https://thumbs.dreamstime.com/b/funchal-%C3%A2%E2%82%AC-madeira-island-portugal-85328664.jpg'}
                      alt={'image Madeira'}/>
             </div>
             <div className={s.descriptionBlock}>
